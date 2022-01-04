29af1d676a4ecfa43eda4e4bddea822f21b6d1af
naziiriah <naziistry7@gmail.com>
2022-01-05 00:22:24 +0100

    changed specs

--- Components/Footer.js
+++ Components/Footer.js
@@ -4,7 +4,7 @@ import { Box, Text } from"@chakra-ui/react";
 const Footer = () => {
     return ( 
         <Box as="footer"    margin="auto" width={"100vw"} maxWidth={"100rem"} height="6rem" bgColor={"navy"}>
-            <Text as="h2" color="#fff">copyright, 2022</Text>
+            <Text as="h5"  fontWeight={"500"} textAlign={"center"} color="#fff">copyright, 2022</Text>
         </Box>
      );
 }
--- Components/Header.js
+++ Components/Header.js
@@ -1,22 +1,11 @@
 import { Box, Text } from "@chakra-ui/react";
-import Link from "next/link";
+import image from "../images/pattern-bg.png"
 
 const Header = () => {
     return (
-        <Box as="header"   margin="auto" width={"100vw"} maxWidth={"100rem"} height="12rem" bgColor={"navy"}>
-            <Box width="90%"marginX={"auto"}  height={["5rem"]} display={"flex"} justifyContent={"space-between"}>
-                <Text as ="h1" textTransform={"capitalize"} fontWeight={"600"} fontSize={"33px"} color="#fff">
-                    reserved.com
-                </Text>
-                
-            </Box>
-            <Box as="nav" height={["5rem"]} width="90%"marginX={"auto"}color="#fff" fontSize={"20px"} display={"flex"} justifyContent={"space-between"}>
-                <Box as="ul" display={"inline-flex"} textDecoration={"none"} listStyleType={"none"}>
-                    
-                    <Link href="/car-rental"><Box as="li" mx="1rem" textTransform={"capitalize"}><Box as="a">flights</Box></Box></Link>
-                </Box>
+        <Box as="header" margin="auto" width={"100vw"} maxWidth={"100rem"} height="12rem" bgImage={image} bgPos={"center"} bgSize={"cover"}>
 
-            </Box>
+
         </Box>
     )
 }
